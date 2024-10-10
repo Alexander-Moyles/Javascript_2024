580318a0f112524195e6756513d0527c67a3ed15
Alexander Moyles <alexander.moyles91@ed.cna.nl.ca>
2024-10-10 12:03:42 -0230

    Class Oct 10th 2024

--- Oct_9_Practice/ReviewStarter/review.js
+++ Oct_9_Practice/ReviewStarter/review.js
@@ -1,10 +1,16 @@
 "use strict";
 
-const today = new Date();
-const year = today.getFullYear();
-const day = today.getDate();
-const month = 1 + today.getMonth();
+document.addEventListener("DOMContentLoaded", () => { //checks to ensure html is loaded first
+    document.querySelector("#button1").addEventListener( "click", () => {
+        const today = new Date();
+        const year = today.getFullYear();
+        let day = today.getDate().toString();
+        let month = (today.getMonth() + 1).toString();
 
-
-document.querySelector("#dateButton");
-alert(`Today's date:\n${year}**${month}**${day}`);
+        alert(`Today's date:\n${year}**${month.padEnd(2,0)}**${day.padEnd(2,0)}`);
+    })
+    document.querySelector("#firstName").addEventListener( "dblclick", (event) => {
+        event.currentTarget.value = "";
+        // or document.querySelector("#firstName").value = "";
+    })
+});
