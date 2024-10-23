dace5779ffa6bb4552f15517858f3b110333762a
Alexander Moyles <alexander.moyles91@ed.cna.nl.ca>
2024-10-23 16:33:31 -0230

    Update test_scores.js
    Continued assignment 4

--- Assignments/Javascript_assign_4/ExtraExercisesA4/ExtraExercisesA4/test_scores/test_scores.js
+++ Assignments/Javascript_assign_4/ExtraExercisesA4/ExtraExercisesA4/test_scores/test_scores.js
@@ -10,8 +10,8 @@ const displayResults = () => {
 	let highestScore = 0;
 	let topStudent;
 
-	for (let i = 0; i < scores; i++) {
-		average += i;
+	for (let i = 0; i < scores.length; i++) {
+		average += scores[i];
 	}
 	average = (average / scores.length);
 
@@ -22,11 +22,17 @@ const displayResults = () => {
 		}
 		count++;
 	}
+	const h2 = document.createElement("h2");
+	const p = document.createElement("p");
 
-	const div = $("#results")
-	div.appendChild("h2");
-	$("#results").firstChild.textContent = "Results";
-	"Average Score = ${average}\nHigh score = ${topStudent} with a score of ${highestScore}`";
+	$("#results").appendChild(h2);	
+	$("#results").firstChild.textContent = `Results`
+
+	$("#results").appendChild(p);
+	$("#results").firstChild.nextElementSibling.textContent = `Average Score = ${average}`
+
+	//$("#results").appendChild(p);
+	//$("#results").firstChild.nextElementSibling.nextElementSibling.textContent = `High score = ${topStudent} with a score of ${highestScore}`;
 };
 
 const addScore = () => {
