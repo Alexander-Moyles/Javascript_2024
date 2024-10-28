47f574e7973e6cde61330b35998d60e5d3fce85c
Alexander-Moyles <alexander.moyles91@ed.cna.nl.ca>
2024-10-27 22:12:25 -0230

    Update faqs.js
    Finished assignment 4
    (for real this time)

--- Assignments/Javascript_assign_4/ExtraExercisesA4/faqs/faqs.js
+++ Assignments/Javascript_assign_4/ExtraExercisesA4/faqs/faqs.js
@@ -5,7 +5,35 @@ const $ = selector => document.querySelector(selector);
 const toggle = evt => {
 	const h2Element = evt.currentTarget;               // get the clicked h2
 	const divElement = h2Element.nextElementSibling;   // get h2's sibling div
+
 	
+	// Before I rechecked the question to see it required an array, I found this solution.
+	// Mostly left it here as I liked how it was shorter, simpler code which accomplished the same effect.
+	// Still works if you uncomment it and comment out everything between it and the empty comment below.
+	/*
+	const h2Elements = document.querySelectorAll("h2.minus");
+	for (let h2 of h2Elements) {
+		if (h2 != h2Element) {
+			h2.classList.toggle("minus")
+			h2.nextElementSibling.classList.toggle("open")
+		}
+	}
+	*/
+
+	const h2Elements = document.querySelectorAll("#faqs h2");
+	let elementArray = [];
+	for (let h2 of h2Elements) {
+		elementArray[elementArray.length] = h2
+	}
+
+	for (let h2 of elementArray) {
+		if (h2 != h2Element) {
+			h2.removeAttribute("class");
+			h2.nextElementSibling.removeAttribute("class");
+		}
+	}
+	//
+
 	h2Element.classList.toggle("minus");
 	divElement.classList.toggle("open");
 	
