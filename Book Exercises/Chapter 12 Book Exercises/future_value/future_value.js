8f18e7a0877b732b5fa376a13388cd949aba1cf7
Alexander Moyles <alexander.moyles91@ed.cna.nl.ca>
2024-11-15 14:28:41 -0330

    a

--- Book Exercises/Chapter 12 Book Exercises/future_value/future_value.js
+++ Book Exercises/Chapter 12 Book Exercises/future_value/future_value.js
@@ -1,5 +1,9 @@
 "use strict";
 
+//TODO finish
+
+const $ = selector => document.querySelector(selector);
+
 const getRandomNumber = max => {
     let rand = null;
     if (!isNaN(max)) {
@@ -14,40 +18,77 @@ const calculateFutureValue = (investment, rate, years) => {
     let futureValue = investment;
     for (let i = 1; i <= years; i++ ) {
         futureValue += futureValue * rate / 100;
+        if (futureValue == Number.POSITIVE_INFINITY) {
+            alert(`Future value = Infinity\ni = ${i}`);
+            i = years;
+        }
     }
+    //alert(`Maximum value in JavaScript is ${Number.MAX_SAFE_INTEGER}`);
     return futureValue.toFixed(2);
 };
 
+const getFormattedValue = (value) => {
+    let periodIndex = value.indexOF('.');
+    console.log(value);
+
+    let cents = value.substring(periodIndex + 1);
+    console.log(cents);
+
+    value = value.substring(value.length - 3);
+    console.log(value);
+
+    let hundreds = value.substring(value.length - 3);
+    console.log(hundreds);
+
+    value = value.substring(0, value.length - 3);
+    console.log(value);
+
+    let thousands = value.substring(value.length - 3);
+    console.log(thousands);
+
+    value = value.substring(0, value.length - 3);
+
+    if (value.length > 0) {
+        let millions = value.substring(value.length - 3);
+        console.log(millions);
+        $("#future_value")
+    }
+}
+
 $(document).ready( () => {
     $("#calculate").click( () => {
-        const investment = parseFloat($("#investment").val());
-        const rate = parseFloat($("#rate").val());
-        const years = parseFloat($("#years").val());
+        const investment = getRandomNumber(10000);//parseFloat($("#investment").val());
+        const rate = getRandomNumber(15);//parseFloat($("#rate").val());
+        const years = Math.random(50);//parseFloat($("#years").val());
+
+        $("#investment").value = investment;
+        $("#rate").value = rate;
+        $("#years").value = years;
 
         let isValid = true;
         if (isNaN(investment) || investment <= 0) {
-            $("#investment").next().text("Must be a valid number greater than 0.");
+            $("#investment").nextElementSibling.textContent = "Must be a valid number greater than 0.";
             isValid = false;
         } else {
-            $("#investment").next().text("");
+            $("#investment").nextElementSibling.textContent = "";
         }
 
         if (isNaN(rate) || rate <= 0) {
-            $("#rate").next().text("Must be a valid number greater than 0.");
+            $("#rate").nextElementSibling.textContent = "Must be a valid number greater than 0.";
             isValid = false;
         } else {
-            $("#rate").next().text("");
+            $("#rate").nextElementSibling.textContent = "";
         }
 
         if (isNaN(years) || years <= 0) {
-            $("#years").next().text("Must be a valid number greater than 0.");
+            $("#years").nextElementSibling.textContent = "Must be a valid number greater than 0.";
             isValid = false;
         } else {
-            $("#years").next().text("");
+            $("#years").nextElementSibling.textContent = "";
         }
 
         if (isValid) {
-            $("#future_value").val(calculateFutureValue(investment, rate, years));
+            $("#future_value").value = calculateFutureValue(investment, rate, years);
         }
     });
     $("#investment").focus();
