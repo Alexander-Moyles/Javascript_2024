a1aca3416cf4fd83d47bb544d9fb5b4a46231cb8
Alexander <alexandermoyles2002@gmail.com>
2024-11-19 20:50:46 -0330

    Update calendar.js
    Worked on Assignment 6

--- Assignments/Javascript_assign_6/extraExercisesA6/calendar/calendar.js
+++ Assignments/Javascript_assign_6/extraExercisesA6/calendar/calendar.js
@@ -18,12 +18,46 @@ const getMonthText = currentMonth => {
 };
 
 const getLastDayofMonth = currentMonth => {
-
+	if (currentMonth === 0 || currentMonth === 2 || currentMonth === 4 || currentMonth === 6 || currentMonth === 7 || currentMonth === 9 || currentMonth === 11) {
+		return 31;
+	}
+	else if (currentMonth === 3 || currentMonth === 5 || currentMonth === 8 || currentMonth === 10) {
+		return 30;
+	}
+	else if (currentMonth === 1) {
+		if (year % 400 == 0 || year % 100 != 0 && year % 4 == 0) {
+			return 29;
+		}
+		else {
+			return 28;
+		}
+	}
 };
 
 document.addEventListener("DOMContentLoaded", ()=> {
-	let tableHeader = "<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>"
-	let firstRow = "<tr><td></td><td></td><td></td><td></td><td></td><td>1</td><td>2</td></tr>";
+	const today = new Date();
+	const November2024 = new Date("Nov 2024");
+	if (today.getMonth() == November2024.getMonth()) {
+		console.log("True");
+	}
 
-	$("#calendar").innerHTML = tableHeader + firstRow;
+	$("#month_year").textContent = `${getMonthText(today.getMonth())} ${today.getFullYear()}`;
+
+	const days = getLastDayofMonth(today.getMonth());
+
+	let i = 1;
+	const daysOfMonth = [];
+	while (i <= days) {
+		daysOfMonth[daysOfMonth.length] = i;
+		i++;
+	}
+
+	let tableHeader = "<tr><th>Sun</th><th>Mon</th><th>Tue</th><th>Wed</th><th>Thu</th><th>Fri</th><th>Sat</th></tr>"
+	let firstWeek = `<tr><td></td><td></td><td></td><td></td><td></td><td>${daysOfMonth[0]}</td><td>${daysOfMonth[1]}</td></tr>`;
+	let secondWeek = `<tr><td>${daysOfMonth[2]}</td><td>${daysOfMonth[3]}</td><td>${daysOfMonth[4]}</td><td>${daysOfMonth[5]}</td><td>${daysOfMonth[6]}</td><td>${daysOfMonth[7]}</td><td>${daysOfMonth[8]}</td></tr>`;
+	let thirdWeek = `<tr><td>${daysOfMonth[9]}</td><td>${daysOfMonth[10]}</td><td>${daysOfMonth[11]}</td><td>${daysOfMonth[12]}</td><td>${daysOfMonth[13]}</td><td>${daysOfMonth[14]}</td><td>${daysOfMonth[15]}</td></tr>`;
+	let fourthWeek = `<tr><td>${daysOfMonth[16]}</td><td>${daysOfMonth[17]}</td><td>${daysOfMonth[18]}</td><td>${daysOfMonth[19]}</td><td>${daysOfMonth[20]}</td><td>${daysOfMonth[21]}</td><td>${daysOfMonth[22]}</td></tr>`;
+	let fifthWeek = `<tr><td>${daysOfMonth[23]}</td><td>${daysOfMonth[24]}</td><td>${daysOfMonth[25]}</td><td>${daysOfMonth[26]}</td><td>${daysOfMonth[27]}</td><td>${daysOfMonth[28]}</td><td>${daysOfMonth[29]}</td></tr>`;
+
+	$("#calendar").innerHTML = tableHeader + firstWeek + secondWeek + thirdWeek + fourthWeek + fifthWeek;
 });
