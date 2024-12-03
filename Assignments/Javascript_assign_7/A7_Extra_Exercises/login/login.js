aeebff89e2f5f22137941508971b2b47ba5e5e4c
Alexander <alexandermoyles2002@gmail.com>
2024-12-03 14:44:21 -0330

    Worked on Assignment 7
    Added 3 of 4 function codes for the first extra in login.js

--- Assignments/Javascript_assign_7/A7_Extra_Exercises/login/login.js
+++ Assignments/Javascript_assign_7/A7_Extra_Exercises/login/login.js
@@ -1,7 +1,21 @@
 "use strict";
 
 const getCookieByName = name => {
-    return "";
+    const cookies = document.cookies;
+    let cookie = cookies.indexOf(name + "=");
+    if (cookie === -1) {
+        return "";
+    }
+    else {
+        cookie += (name.length + 1);
+        let end = cookies.indexOf(";", start);
+        if (end === -1) {
+            end = cookies.length;
+        }
+
+        const cookieValue = cookies.substring(start, end);
+        return decodeURIComponent(cookieValue);
+    }
 };
 
 const setCookie = (name, value, days) => {
@@ -9,9 +23,9 @@ const setCookie = (name, value, days) => {
 };
 
 const deleteCookie = name => {
-
+    document.cookie = name + "=''; max-age=0; path=/";
 };
 
 const goToPage = url => {
-
+    location.href = url;
 };
