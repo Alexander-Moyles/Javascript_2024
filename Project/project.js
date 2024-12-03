e5300f1854aeb3ad2ee2db06f690be097ea2a06a
Alexander <alexandermoyles2002@gmail.com>
2024-12-02 20:36:59 -0330

    Updated Project
    + Fixed button sizing issue
    + Added more questions
    + Added more images

--- Project/project.js
+++ Project/project.js
@@ -1,14 +1,42 @@
 "use strict"
 const $ = (selector) => document.querySelector(selector);
 
-const questions = ["Which of the 13 Primes first appeared in the Generation 2 Comic?", "Which of the 13 Primes is the true identity of the barkeep Maccaddam?"];
-const questionOne = ["Prima", "The Fallen", "Liege Maximo", "Solus Prime", "c"];
-const questionTwo = ["Amalgamous Prime", "Quintus Prime", "Micronus Prime", "Alchemist Prime", "d"];
-const questionThree = ["", "", "", ""];
-const questionFour = ["", "", "", ""];
-const questionFive = ["", "", "", ""];
-const questionSix = ["", "", "", ""];
-const answers = [questionOne, questionTwo/*, questionThree, questionFour, questionFive, questionSix*/];
+const questionList = [
+    ["Which of the 13 Primes first appeared in the Generation 2 Comic?",
+        "Prima", "The Fallen", "Liege Maximo", "Solus Prime", "c"],
+    ["Which of the 13 Primes is the true identity of the barkeep Maccadam?",
+        "Amalgamous Prime", "Quintus Prime", "Micronus Prime", "Alchemist Prime", "d", 0],
+    ["Who was the third of the original 13 Primes?",
+        "Alpha Trion", "Vector Prime", "Onyx Prime", "The Arisen", "a"],
+    ["Which of the Primes is this?",
+        "The Fallen", "Micronus Prime", "Solus Prime", "Onyx Prime", "d", 1],
+    ["What is The Fallen's true name?",
+        "Shockwave", "Megatronus", "Jetfire", "Twinferno", "b", 4],
+    ["Which of these Autobots does the Mistress of Flame believe to be the reincarnated Thirteenth Prime/The Arisen?",
+        "Bumblebee", "Optimus Prime", "Windblade", "Skytop", "b", 5],
+    ["Which of the 13 Primes does this symbol represent?",
+        "Solus Prime", "Liege Maximo", "Micronus Prime", "Vector Prime", "a", 3
+    ],
+    ["Which of the 13 Primes does this symbol represent?",
+        "Solus Prime", "The Fallen", "Micronus Prime", "Prima", "c", 2
+    ],
+    ["What is the name of Prima Prime's Sword?",
+        "The Cyber Calibre", "The Vorpal Blade", "Rhisling", "The Star Saber", "d", 6
+    ],
+    ["Which of the 13 Primes was killed by Galvatron, and the first prime to be killed by a non-prime in the 2005 IDW Comic run?",
+        "Nexus Prime", "Alchemist Prime", "Solus Prime", "Micronus Prime", "a"
+    ]
+];
+
+const shuffleQuestions = (array) => {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+}
+const questions = shuffleQuestions(questionList);
+console.log(questions);
 
 let currentQuestion = 1;
 let totalQuestions = questions.length;
@@ -21,26 +49,15 @@ const setStats = () => {
     $("#question_count").textContent = `${currentQuestion} / ${totalQuestions}`;
 };
 
-const nextQuestion = () => {
-    $("#question").firstChild.nextElementSibling.textContent = questions[currentQuestion - 1];
-    $("#a").textContent = answers[currentQuestion - 1][0];
-    $("#b").textContent = answers[currentQuestion - 1][1];
-    $("#c").textContent = answers[currentQuestion - 1][2];
-    $("#d").textContent = answers[currentQuestion - 1][3];
-    if (answers[currentQuestion - 1][5] != null) {
-        //TODO: image setting code
-    }
-};
-
 const wrongHelper = () => {
     for(let i = 0; i < 4; i++) {
-        if (answerlist[i].id != answers[currentQuestion - 1][4]) {
+        if (answerlist[i].id != questions[currentQuestion - 1][5]) {
             $(`#${answerlist[i].id}`).addEventListener("click", () => {
                 answerlist[i].classList.toggle("wrong");
                 answerlist[i].parentElement.classList.toggle("wrong");
                 correctHelper();
             });
-        } //TODO: Disable buttons after answer is chosen
+        } //TODO: Disable buttons after pressing
         else {
             $(`#${answerlist[i].id}`).addEventListener("click", () => {
             answerlist[i].classList.toggle("correct");
@@ -54,7 +71,7 @@ const wrongHelper = () => {
 
 const correctHelper = () => {
     for(let i = 0; i < 4; i++) {
-        if (answerlist[i].id == answers[currentQuestion - 1][4]) {
+        if (answerlist[i].id == questions[currentQuestion - 1][5]) {
             answerlist[i].classList.toggle("correct");
             answerlist[i].parentElement.classList.toggle("correct");
         }
@@ -62,13 +79,36 @@ const correctHelper = () => {
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-    //TODO: preload images
+    const images = document.querySelectorAll("a");
+    const imageCache = [];
+    let img;
+    for (let image of images) {
+        img = new Image();
+        img.src = image.href;
+        img.alt = image.alt;
+
+        imageCache[imageCache.length] = img;
+    }
+
+    const nextQuestion = () => {
+        $("#question").firstChild.nextElementSibling.textContent = questions[currentQuestion - 1][0];
+        $("#a").textContent = questions[currentQuestion - 1][1];
+        $("#b").textContent = questions[currentQuestion - 1][2];
+        $("#c").textContent = questions[currentQuestion - 1][3];
+        $("#d").textContent = questions[currentQuestion - 1][4];
+        if (questions[currentQuestion - 1][6] != null) {
+            $("#picture").src = `${imageCache[questions[currentQuestion - 1][6]].src}`;
+            $("#picture").alt = `${imageCache[questions[currentQuestion - 1][6]].alt}`;
+        }
+        else {
+            $("#picture").src = "";
+        }
+    };
+
     setStats();
     nextQuestion();
 
     if (currentQuestion = 1) {
         wrongHelper();
     }
-
-    
 })
